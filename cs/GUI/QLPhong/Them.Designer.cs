dc182e37c0d4f465a927b8dbab796efb7d21f1dd
Bao <nguyengiabao91753@gmail.com>
2025-03-30 11:30:26 +0700

    Summit GUI FROM Phong

---
+++ cs/GUI/QLPhong/Them.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace cs.GUI.QLPhong
+{
+    partial class Them
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelThongTinPhong = new System.Windows.Forms.Panel();
+            buttonSave = new System.Windows.Forms.Button();
+            comboBoxTrangThai = new System.Windows.Forms.ComboBox();
+            textBoxSucChua = new System.Windows.Forms.TextBox();
+            textTenPhong = new System.Windows.Forms.TextBox();
+            textMaPhong = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            buttonChangeImage = new System.Windows.Forms.Button();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            panel5 = new System.Windows.Forms.Panel();
+            labelname = new System.Windows.Forms.Label();
+            label14 = new System.Windows.Forms.Label();
+            labelLastTimeUpdate = new System.Windows.Forms.Label();
+            panelThongTinPhong.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelThongTinPhong
+            // 
+            panelThongTinPhong.Controls.Add(labelLastTimeUpdate);
+            panelThongTinPhong.Controls.Add(label14);
+            panelThongTinPhong.Controls.Add(buttonSave);
+            panelThongTinPhong.Controls.Add(comboBoxTrangThai);
+            panelThongTinPhong.Controls.Add(textBoxSucChua);
+            panelThongTinPhong.Controls.Add(textTenPhong);
+            panelThongTinPhong.Controls.Add(textMaPhong);
+            panelThongTinPhong.Controls.Add(label4);
+            panelThongTinPhong.Controls.Add(label3);
+            panelThongTinPhong.Controls.Add(label2);
+            panelThongTinPhong.Controls.Add(label1);
+            panelThongTinPhong.Controls.Add(buttonChangeImage);
+            panelThongTinPhong.Controls.Add(pictureBox1);
+            panelThongTinPhong.Controls.Add(panel5);
+            panelThongTinPhong.Location = new System.Drawing.Point(1, 0);
+            panelThongTinPhong.Name = "panelThongTinPhong";
+            panelThongTinPhong.Size = new System.Drawing.Size(689, 439);
+            panelThongTinPhong.TabIndex = 0;
+            // 
+            // buttonSave
+            // 
+            buttonSave.BackColor = System.Drawing.Color.ForestGreen;
+            buttonSave.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            buttonSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            buttonSave.Location = new System.Drawing.Point(502, 356);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new System.Drawing.Size(179, 55);
+            buttonSave.TabIndex = 11;
+            buttonSave.Text = "Lưu";
+            buttonSave.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxTrangThai
+            // 
+            comboBoxTrangThai.FormattingEnabled = true;
+            comboBoxTrangThai.Location = new System.Drawing.Point(389, 241);
+            comboBoxTrangThai.Name = "comboBoxTrangThai";
+            comboBoxTrangThai.Size = new System.Drawing.Size(168, 28);
+            comboBoxTrangThai.TabIndex = 10;
+            // 
+            // textBoxSucChua
+            // 
+            textBoxSucChua.Location = new System.Drawing.Point(389, 200);
+            textBoxSucChua.Name = "textBoxSucChua";
+            textBoxSucChua.Size = new System.Drawing.Size(168, 27);
+            textBoxSucChua.TabIndex = 9;
+            // 
+            // textTenPhong
+            // 
+            textTenPhong.Location = new System.Drawing.Point(389, 156);
+            textTenPhong.Name = "textTenPhong";
+            textTenPhong.Size = new System.Drawing.Size(168, 27);
+            textTenPhong.TabIndex = 8;
+            // 
+            // textMaPhong
+            // 
+            textMaPhong.Location = new System.Drawing.Point(389, 111);
+            textMaPhong.Name = "textMaPhong";
+            textMaPhong.Size = new System.Drawing.Size(168, 27);
+            textMaPhong.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label4.Location = new System.Drawing.Point(276, 242);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(91, 23);
+            label4.TabIndex = 6;
+            label4.Text = "Trạng thái:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label3.Location = new System.Drawing.Point(276, 200);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(85, 23);
+            label3.TabIndex = 5;
+            label3.Text = "Sức chứa:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label2.Location = new System.Drawing.Point(276, 156);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(95, 23);
+            label2.TabIndex = 4;
+            label2.Text = "Tên phòng:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            label1.Location = new System.Drawing.Point(276, 111);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(94, 23);
+            label1.TabIndex = 3;
+            label1.Text = "Mã phòng:";
+            // 
+            // buttonChangeImage
+            // 
+            buttonChangeImage.BackColor = System.Drawing.SystemColors.Highlight;
+            buttonChangeImage.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            buttonChangeImage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            buttonChangeImage.Location = new System.Drawing.Point(20, 356);
+            buttonChangeImage.Name = "buttonChangeImage";
+            buttonChangeImage.Size = new System.Drawing.Size(179, 55);
+            buttonChangeImage.TabIndex = 2;
+            buttonChangeImage.Text = "Đổi Ảnh";
+            buttonChangeImage.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new System.Drawing.Point(20, 83);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(179, 247);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = System.Drawing.SystemColors.HotTrack;
+            panel5.Controls.Add(labelname);
+            panel5.Location = new System.Drawing.Point(-1, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new System.Drawing.Size(829, 63);
+            panel5.TabIndex = 0;
+            // 
+            // labelname
+            // 
+            labelname.AutoSize = true;
+            labelname.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            labelname.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            labelname.Location = new System.Drawing.Point(4, 15);
+            labelname.Name = "labelname";
+            labelname.Size = new System.Drawing.Size(337, 31);
+            labelname.TabIndex = 0;
+            labelname.Text = "THÔNG TIN PHÒNG - Tạo mới";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.ForeColor = System.Drawing.Color.Red;
+            label14.Location = new System.Drawing.Point(267, 287);
+            label14.Name = "label14";
+            label14.Size = new System.Drawing.Size(220, 20);
+            label14.TabIndex = 12;
+            label14.Text = "Thông tin cập nhật lần cuối vào:";
+            // 
+            // labelLastTimeUpdate
+            // 
+            labelLastTimeUpdate.AutoSize = true;
+            labelLastTimeUpdate.ForeColor = System.Drawing.Color.Red;
+            labelLastTimeUpdate.Location = new System.Drawing.Point(493, 287);
+            labelLastTimeUpdate.Name = "labelLastTimeUpdate";
+            labelLastTimeUpdate.Size = new System.Drawing.Size(62, 20);
+            labelLastTimeUpdate.TabIndex = 13;
+            labelLastTimeUpdate.Text = "xxx xxxx";
+            // 
+            // Them
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(panelThongTinPhong);
+            Name = "Them";
+            Size = new System.Drawing.Size(686, 441);
+            panelThongTinPhong.ResumeLayout(false);
+            panelThongTinPhong.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelThongTinPhong;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonChangeImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxTrangThai;
+        private System.Windows.Forms.TextBox textBoxSucChua;
+        private System.Windows.Forms.TextBox textTenPhong;
+        private System.Windows.Forms.TextBox textMaPhong;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label labelLastTimeUpdate;
+    }
+}
